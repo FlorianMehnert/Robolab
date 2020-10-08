c4397ebbfd8b4bdbb6e232a71b6e8cbc32195ff3
Florian Mehnert <florian.mehnert@gmail.com>
2020-10-08 10:57:25 +0200

    added follow including calbrate to set colorsensor

---
+++ src/follow.py
@@ -0,0 +1,85 @@
+from time import sleep
+
+import ev3dev.ev3 as ev3
+
+
+class Follow:
+    def __init__(self, m1:ev3.LargeMotor, m2:ev3.LargeMotor, cs:ev3.ColorSensor, ts:ev3.TouchSensor):
+        self.m1 = m1
+        self.m2 = m2
+        self.cs = cs
+        self.ts = ts
+
+    def rgbToRefl(self, r, g, b ):
+        return (r + g + b) / 3
+
+    def calibrate(self):
+        self.cs.mode = "RGB-RAW"
+
+        while not self.ts.is_pressed:
+            sleep(0.1)
+
+        while self.ts.is_pressed:
+            sleep(0.1)
+
+        rgbWhite = self.cs.bin_data("hhh")
+        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
+        ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
+
+        while not self.ts.is_pressed:
+            sleep(0.1)
+
+        while self.ts.is_pressed:
+            sleep(0.1)
+
+        rgbBlack = self.cs.bin_data("hhh")
+        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.AMBER)
+        ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.AMBER)
+
+        while not self.ts.is_pressed:
+            sleep(0.1)
+
+        while self.ts.is_pressed:
+            sleep(0.1)
+
+        rgbRed = self.cs.bin_data("hhh")
+        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
+        ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
+
+        while not self.ts.is_pressed:
+            sleep(0.1)
+
+        while self.ts.is_pressed:
+            sleep(0.1)
+
+        rgbBlue = self.cs.bin_data("hhh")
+        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
+        ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
+
+        reflWhite = self.rgbToRefl(*rgbWhite)
+        reflBlack = self.rgbToRefl(*rgbBlack)
+
+        print("Black: " + str(reflBlack))
+        print("White: " + str(reflWhite))
+
+        while not self.ts.is_pressed:
+            sleep(0.1)
+
+        while self.ts.is_pressed:
+            sleep(0.1)
+
+        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.AMBER)
+        ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.AMBER)
+
+        sleep(0.5)
+
+        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
+        ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
+
+        optimal = (reflWhite + reflBlack) / 2
+        return rgbRed, rgbBlue, rgbWhite, rgbBlack, optimal
+
+    def follow(self, optimal, baseSpeed):
+        self.m1.run_forever(speed_sp = baseSpeed)
+        self.m2.run_forever(speed_sp = baseSpeed)
+
